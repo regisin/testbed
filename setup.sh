2a025e4c9461ad3666acef71bb72b1f5cd6e2269
Paulo Alexandre Regis <regisin@gmail.com>
2017-06-14 19:27:18 -0700

    Update the way we get the node id

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 before_reboot(){
+  node_id=0
+  re='^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-5][0-4])$'
+  while ! [[ $node_id =~ $re ]]
+  do
+    echo "What is the number of this node? [1-254]: "
+    read node_id
+  done
+  
+  
   cd /var/tmp
   
   echo "Enable autologin"
@@ -8,19 +17,6 @@ before_reboot(){
   
   echo "Enable ssh"
   sudo systemctl enable ssh.socket
-  
-  echo "Copying template files"
-  cd /var/tmp
-  wget https://raw.githubusercontent.com/regisin/testbed/master/templates/interfaces
-  sudo cp /var/tmp/interfaces /etc/network/interfaces
-  wget https://raw.githubusercontent.com/regisin/testbed/master/templates/rc.local
-  sudo cp /var/tmp/rc.local /etc/rc.local
-  wget https://raw.githubusercontent.com/regisin/testbed/master/templates/keyboard
-  sudo cp /var/tmp/keyboard /etc/default/keyboard
-  
-  echo "Renaming node, input required"
-  wget https://raw.githubusercontent.com/regisin/testbed/master/name_this_node.py
-  sudo python2.7 name_this_node.py
   
   echo "apt-get update"
   sudo apt-get update
@@ -40,6 +36,19 @@ before_reboot(){
   cd rtl8812au-1
   make
   sudo make install
+  
+  echo "Copying template files"
+  cd /var/tmp
+  wget https://raw.githubusercontent.com/regisin/testbed/master/templates/interfaces
+  sudo cp /var/tmp/interfaces /etc/network/interfaces
+  wget https://raw.githubusercontent.com/regisin/testbed/master/templates/rc.local
+  sudo cp /var/tmp/rc.local /etc/rc.local
+  wget https://raw.githubusercontent.com/regisin/testbed/master/templates/keyboard
+  sudo cp /var/tmp/keyboard /etc/default/keyboard
+  
+  echo "Renaming node, input required"
+  wget https://raw.githubusercontent.com/regisin/testbed/master/name_this_node.py
+  sudo python2.7 name_this_node.py --node-id $node_id
   
   echo "Removing files"
   cd ~
