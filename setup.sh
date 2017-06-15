daa75679fef294bb54c458d65f84b8faf5584598
Paulo Alexandre Regis <regisin@gmail.com>
2017-06-14 17:27:28 -0700

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -4,21 +4,21 @@ before_reboot(){
   cd /var/tmp
   
   echo "Enable autologin"
-	sudo ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
+  sudo ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
   
   echo "Enable ssh"
   sudo systemctl enable ssh.socket
-
-	echo "apt-get update"
-	sudo apt-get update
-
+  
+  echo "apt-get update"
+  sudo apt-get update
+  
   echo "Removing wpa suplicant"
   sudo apt-get remove -y wpasuplicant
   
-	echo "Installing dependencies"
-	sudo apt-get install -y bc libncurses5-dev git mercurial tcpdump tshark
-	
-  echo "Acquiring kernel headers"
+  echo "Installing dependencies"
+  sudo apt-get install -y bc libncurses5-dev git mercurial tcpdump tshark
+  
+  echo "Getting kernel headers"
   sudo wget https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/bin/rpi-source && sudo chmod +x /usr/bin/rpi-source && /usr/bin/rpi-source -q --tag-update
   rpi-source
   
@@ -30,35 +30,22 @@ before_reboot(){
   
   echo "Copying template files"
   cd /var/tmp
-	wget https://raw.githubusercontent.com/regisin/testbed/master/templates/interfaces
-	sudo cp /var/tmp/interfaces /etc/network/interfaces
-  
+  wget https://raw.githubusercontent.com/regisin/testbed/master/templates/interfaces
+  sudo cp /var/tmp/interfaces /etc/network/interfaces
   wget https://raw.githubusercontent.com/regisin/testbed/master/templates/rc.local
   sudo cp /var/tmp/rc.local /etc/network/rc.local
-}
 
-after_reboot(){
-  cd /var/tmp
-  
+  echo "Renaming node, input required"
   wget https://raw.githubusercontent.com/regisin/testbed/master/name_this_node.py
-  
-  echo "Updating static IP and hostname"
-	sudo python2.7 name_this_node.py
+  sudo python2.7 name_this_node.py
   
   cd /var/tmp
   echo "Removing files"
-	rm name_this_node.py
+  rm name_this_node.py
   rm interfaces
   rm rc.local
 }
 
-if [ -f /var/tmp/rebooting-for-updates ]; then
-    after_reboot
-    rm /var/tmp/rebooting-for-updates
-    echo "Rebooting to make effect"
-    sudo reboot
-else
-    before_reboot
-    touch /var/tmp/rebooting-for-updates
-    sudo reboot
-fi
+before_reboot
+echo "Rebooting to make effect"
+sudo reboot
